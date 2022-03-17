8660b620e33a216f1374d8f5e493f84680cf87ed
aidanliddiard <aidanliddiard@gmail.com>
2022-03-17 14:58:26 -0700

    card set up

--- src/Components/PokeCard/PokeCard.js
+++ src/Components/PokeCard/PokeCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './PokeCard.css';
 
-export default function PokeCard({ pokemon, type_1, type_2, url_image }) {
+export default function PokeCard({ pokemon, type_1, type_2, url_image, ability_1, egg_group_1 }) {
   return (
     <div className={`${type_1} card`}>
       <h2 className="name">{pokemon}</h2>
       <img src={url_image} />
-      <p>
-        ({type_1}, {type_2})
-      </p>
+      <p>Type 1: {type_1}</p>
+      <p>Type 2: {type_2}</p>
+      <p>Ability: {ability_1}</p>
+      <p>Egg group: {egg_group_1}</p>
     </div>
   );
 }
